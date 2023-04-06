c9b58f5153d88d0bc6b49d5cd2ae39bfb0b26c6f
AbelF93 <abel.fortunati@gmail.com>
2023-04-06 12:34:32 +0200

    exercice 1.0

---
+++ JScripts/1.0.js
@@ -0,0 +1,7 @@
+let valeurBasique = 25
+let valeurAppliquée = 2
+console.log(valeurBasique+valeurAppliquée);
+console.log(valeurBasique-valeurAppliquée);
+console.log(valeurBasique*valeurAppliquée);
+console.log(valeurBasique/valeurAppliquée);
+console.log(valeurBasique%valeurAppliquée);
