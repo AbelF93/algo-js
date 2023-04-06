c73af62596cd98157e4a251144eb8c5c16d7fe38
AbelF93 <abel.fortunati@gmail.com>
2023-04-06 14:36:16 +0200

    Exercice 2.2

---
+++ JScripts/2.2.js
@@ -0,0 +1,9 @@
+let min = prompt('Enter a number');
+let max = prompt ('Enter an other value');
+let current = prompt ('Last one!');
+if ((current>min) && (current<max)) {
+    alert("You're between "+min+' and '+max+ "!");
+}
+else if (current) {
+    alert("you sucks");
+}
