ab04e03c41e1365b927498b6963e876e140cb8ff
AbelF93 <abel.fortunati@gmail.com>
2023-09-14 10:10:29 +0200

    all exercices

--- JScripts/1.2.js
+++
@@ -1,4 +0,0 @@
-let name = "Lambert";
-let firstname = "Gerard";
-let city ="Paris";
-console.log("Your name is "+ firstname + ' ' + name + " and you live in " + city + '.');
--- JScripts/2.1.js
+++
@@ -1,7 +0,0 @@
-let age = prompt("what's your age ?");
-if (age >= 18){
-    alert("You're an adult");
-}
-else if(age){
-    alert("You're not an adult");
-}
